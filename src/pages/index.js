d19a4ccc72fecfef2df1d104cd571b5bd06a5c61
c50a326 <c50a326@tutanota.com>
2018-05-03 13:28:56 +0545

    Fixed HTML.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,11 @@ export default () => (
     <h1>Heading</h1>
     <div>
       <p>
-        Ipsum aperiam expedita illum dolores placeat, molestias perspiciatis? Quasi mollitia omnis illo eius numquam Quo velit earum enim <a href="http://google.com">culpa</a>hic amet? Possimus ab labore iste dignissimos mollitia Ab est nihil.
+        Ipsum aperiam expedita illum dolores placeat, molestias perspiciatis? Quasi mollitia omnis illo eius numquam Quo velit earum enim <a href="http://google.com">culpa</a> hic amet? Possimus ab labore iste dignissimos mollitia Ab est nihil.
       </p>
       <blockquote>
         <p>
-          Dolor tenetur reiciendis explicabo ratione soluta repudiandae earum Doloribus mollitia ut dicta dicta nemo? Aspernatur aperiam explicabo nisi animi blanditiis. Recusandae inventore expedita tempora perspiciatis repudiandae Voluptatibus dicta adipisci recusandae!
-          <em>Ipsum sapiente itaque ad minima beatae Earum architecto maxime cumque ipsa itaque Quo vel eveniet ut inventore aspernatur Illo beatae aliquid libero ad autem Repudiandae consequatur laborum ipsum a possimus</em>
-          Elit quis temporibus eaque natus iusto vel Obcaecati assumenda aperiam quae mollitia repellendus explicabo? In cum ad id sunt nisi, provident architecto similique. Magni aspernatur earum necessitatibus itaque adipisci! Illum?
+          Dolor tenetur reiciendis explicabo ratione soluta repudiandae earum Doloribus mollitia ut dicta dicta nemo? Aspernatur aperiam explicabo nisi animi blanditiis. Recusandae inventore expedita tempora perspiciatis repudiandae Voluptatibus dicta adipisci recusandae! <em>Ipsum sapiente itaque ad minima beatae Earum architecto maxime cumque ipsa itaque Quo vel eveniet ut inventore aspernatur Illo beatae aliquid libero ad autem Repudiandae consequatur laborum ipsum a possimus.</em> Elit quis temporibus eaque natus iusto vel Obcaecati assumenda aperiam quae mollitia repellendus explicabo? In cum ad id sunt nisi, provident architecto similique. Magni aspernatur earum necessitatibus itaque adipisci! Illum?
         </p>
       </blockquote>
     </div>
