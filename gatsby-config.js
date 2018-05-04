a5601793cb6ed28c1227a75a45681f38f1badc74
c50a326 <c50a326@tutanota.com>
2018-05-04 11:27:35 +0545

    Added grand-view typography plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,10 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-typography'
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: `src/utils/typography`
+      }
+    }
   ]
 };
